3affe780d81e2720208edb3eb12d33ebb7968b77
nancyliuyang <liu_yangchina@126.com>
2022-09-14 18:50:28 +0800

    add log

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,22 @@ import { DefaultSeo } from "next-seo"
 import theme from "theme"
 import "../styles/prism.css"
 import siteConfig from "site.config"
+import mixgather from "mixgather"
+import { useEffect } from "react"
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    mixgather.init({
+      debug: process.env.NODE_ENV !== "production",
+      appName: "app.mirrorworld.fun",
+      google: {
+        id: "G-QTVY981GJR",
+      },
+      mixpanel: {
+        token: "6a688b6265a76149d39d340730749870",
+      },
+    })
+  })
   return (
     <ChakraProvider theme={theme}>
       <DefaultSeo {...siteConfig.seo} />
