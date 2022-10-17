9c887dc0d70ac16cced267d8218cacb310850f99
codebender828 <jbakebwa@163.com>
2022-10-17 04:54:01 -0700

    feat: create blog links

--- sidebar.config.ts
+++ sidebar.config.ts
@@ -51,6 +51,14 @@ const sidebar: Record<"docs", SidebarItem[]> = {
           isExternal: true,
           href: "https://developer.mirrorworld.fun",
         },
+        {
+          type: "doc",
+          label: "Blog",
+          id: "blog",
+          new: true,
+          isExternal: true,
+          href: "/blog",
+        },
       ],
     },
     {
@@ -139,6 +147,14 @@ const sidebar: Record<"docs", SidebarItem[]> = {
       icon: HiOutlineViewGrid,
       id: "resources",
       items: [
+        {
+          type: "doc",
+          label: "Blog",
+          id: "blog",
+          new: true,
+          isExternal: true,
+          href: "/blog",
+        },
         { type: "doc", label: "Support", id: "support" },
         { type: "doc", label: "Tutorials", id: "tutorials" },
         { type: "doc", label: "Guides", id: "guides" },
