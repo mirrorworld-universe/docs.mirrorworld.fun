7cacd603e63cf06f57c714e1e5685b65ae6f84fe
codebender828 <jbakebwa@163.com>
2023-01-18 10:51:10 +0800

    feat: conform all route redirects

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,35 @@
 import { NextResponse } from "next/server"
 import type { NextRequest } from "next/server"
 
+const routeMapping = {
+  "/js/js-installation": "/integration/js",
+  "/js/js-api": "/api-reference/js",
+  "/js/js-authentication": "/api-reference/js/authentication",
+  "/ios/ios-installation": "/integration/ios",
+  "/ios/ios-api": "/api-reference/ios",
+  "/ios/ios-authentication": "/api-reference/ios/authentication",
+  "/android/android-installation": "/integration/android",
+  "/android/android-api": "/api-reference/android",
+  "/android/android-authentication": "/api-reference/android/authentication",
+  "/unity/unity-installation": "/integration/unity",
+  "/unity/unity-api": "/api-reference/unity",
+  "/unity/unity-authentication": "/api-reference/unity/authentication",
+  "/rust/rust-installation": "/integration/rust",
+  "/rust/rust-api": "/api-reference/rust",
+  "/rust/rust-authentication": "/api-reference/rust/authentication",
+}
+
 export function middleware(request: NextRequest) {
   const url = request.nextUrl.clone()
-  console.log(url)
   if (["/", "/overview"].includes(url.pathname)) {
     url.pathname = "/overview/introduction"
+    return NextResponse.redirect(url)
+  }
+
+  const targetPath = routeMapping[url.pathname]
+
+  if (targetPath) {
+    url.pathname = targetPath
     return NextResponse.redirect(url)
   }
 }
