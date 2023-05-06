4a8a69d3ff4656003865976dde85e4b0dee5827f
wklili <18136225579@163.com>
2023-05-06 18:24:03 +0800

    feat: Menu structure adjustment

---
+++ pages/resources/[[...slug]].tsx
@@ -0,0 +1,28 @@
+import { useMDX } from "components/mdx-components"
+import SEO from 'components/seo'
+import { Resources } from "contentlayer/generated"
+import DocsLayout from "layouts/docs"
+import { getResourcesDoc, getResourcesPaths } from "lib/contentlayer-utils"
+import { generateShareImageUrl } from 'lib/seo'
+import { GetStaticPaths, GetStaticProps } from "next"
+
+export default function OverviewPage({ doc }: { doc: Resources }) {
+  const Component = useMDX(doc.body.code)
+  const imageUrl = generateShareImageUrl({ title: doc.title, description: doc.description })
+  return (
+    <>
+      <SEO title={doc.title} description={doc.description} />
+      <DocsLayout doc={doc}>{Component}</DocsLayout>
+    </>
+  )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return { paths: getResourcesPaths(), fallback: false }
+}
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  const params = ctx.params as any
+  const pagePath = params.slug?.join("/") ?? ""
+  return { props: { doc: getResourcesDoc(pagePath) } }
+}
